c6d9cff434b5253c09ea9d979f9682d8f99c9515
anandversespace-debug <anandverse.space@gmail.com>
2026-04-18 03:38:49 +0530

    vercel . json

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { SettingsProvider } from './context/SettingsContext'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
+    <SpeedInsights />
     <AuthProvider>
       <SettingsProvider>
         <App />
